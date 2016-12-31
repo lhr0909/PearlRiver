c62271af36780aa9a43ee7d517f0bdbe2f314d88
Simon Liang <simon@divby0.io>
2016-12-31 15:50:59 +0800

    kafka stream source set up

--- src/main/scala/com/hoolix/processor/XYZProcessorMain.scala
+++ src/main/scala/com/hoolix/processor/XYZProcessorMain.scala
@@ -1,9 +1,15 @@
 package com.hoolix.processor
 
-import akka.actor.{ActorSystem, Props}
-import akka.routing.FromConfig
-import com.hoolix.processor.actors.KafkaStreamFetcher
+import akka.actor.{ActorSystem}
+import akka.kafka.{ConsumerSettings, Subscriptions}
+import akka.kafka.scaladsl.Consumer
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
 import com.typesafe.config.ConfigFactory
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer}
+
+import scala.concurrent.Future
 
 
 /**
@@ -13,9 +19,20 @@ import com.typesafe.config.ConfigFactory
 object XYZProcessorMain extends App {
   override def main(args: Array[String]): Unit = {
     val config = ConfigFactory.load
-    val system = ActorSystem("xyz-processor", config)
-    val fetchers = system.actorOf(FromConfig.props(Props[KafkaStreamFetcher]), "fetchers")
-
-
+    implicit val system = ActorSystem("xyz-processor", config)
+    implicit val materializer = ActorMaterializer()
+    val consumerSettings = ConsumerSettings(system, new ByteArrayDeserializer, new StringDeserializer)
+      .withBootstrapServers("0.0.0.0:9092")
+      .withGroupId("xyz-processor")
+      .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+    val done = Consumer.committableSource(consumerSettings, Subscriptions.topics("hooli_topic"))
+        .mapAsync(1) { msg =>
+          println(msg.record.value)
+          Future.successful(msg)
+        }
+        .mapAsync(1) { msg =>
+          msg.committableOffset.commitScaladsl()
+        }
+        .runWith(Sink.ignore)
   }
 }
--- src/main/scala/com/hoolix/processor/actors/KafkaStreamFetcher.scala
+++ src/main/scala/com/hoolix/processor/actors/KafkaStreamFetcher.scala
@@ -1,13 +1,8 @@
 package com.hoolix.processor.actors
 
-import akka.actor.Actor
-
 /**
   * Hoolix 2016
   * Created by simon on 12/30/16.
   */
-class KafkaStreamFetcher extends Actor {
-  override def receive = {
-
-  }
+class KafkaStreamFetcher {
 }
