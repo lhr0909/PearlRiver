ba798bfdfbe942e1521d02b2c79a3d3033eaa34b
Simon Liang <simon@divby0.io>
2017-01-07 16:45:38 +0800

    package and run scripts are ready

--- bin/package.sh
+++ bin/package.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-pkg="target/scala-2.10/XYZ-Pipeline-assembly-1.0.jar"
-release="$(awk -F= '/^pipeline_version/{print $2}' gradle.properties)"
-build_pkg="build/libs/xyz-pipeline-$release.jar"
+#pkg="target/scala-2.10/XYZ-Pipeline-assembly-1.0.jar"
+#release="$(awk -F= '/^pipeline_version/{print $2}' gradle.properties)"
 
-mkdir -p target/scala-2.10/
+pkg="dist/XYZ-Processor.jar"
+release="0.1"
+build_pkg="build/libs/xyz-processor-$release.jar"
 
 ./gradlew shadowJar $@ || exit 1
+
+mkdir -p dist
+
 cp -f $build_pkg $pkg
 
-tar -czvf     xyz-pipeline-$release.tar \
-    --exclude bin/submit.sh \
-    --exclude conf/spark-defaults.conf \
-    --exclude conf/spark-defaults-hbase2es.conf \
-    --exclude conf/env.sh \
-    --exclude conf/pipeline.yml \
-    --exclude conf/spark-online.conf \
-    --exclude conf/es-online-mapping.json \
+tar -czvf xyz-processor-$release.tar \
+    --exclude bin/package.sh \
     $pkg \
-    conf/*.template \
-    conf/log4j* \
-    conf/patterns \
-    conf/GeoLite2-City.mmdb \
-    demo \
-    bin/submit* \
-    bin/toolbox.sh || exit 1
+    conf/* \
+    bin/* || exit 1
 
 echo done
--- bin/run.sh
+++ bin/run.sh
@@ -3,6 +3,6 @@
 NOHUP_LOG=xyz-processor.log
 NOHUP_PID=xyz-processor.pid
 
-nohup java -jar xyz-processor-0.1.jar $@ >> $NOHUP_LOG 2>&1 &
+nohup java -jar dist/XYZ-Processor.jar $@ >> $NOHUP_LOG 2>&1 &
 
 echo $! > $NOHUP_PID
