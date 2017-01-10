0a3fe0a61a48c89d7110d790c5b19b332fe19e06
peiyuchao <bladeralien@126.com>
2017-01-10 19:18:02 +0800

    custom mapping

--- src/main/scala/com/hoolix/processor/flows/CreateIndexFlow.scala
+++ src/main/scala/com/hoolix/processor/flows/CreateIndexFlow.scala
@@ -5,13 +5,17 @@ import java.util.concurrent.TimeUnit
 import akka.NotUsed
 import akka.stream.scaladsl.Flow
 import com.hoolix.processor.models.KafkaTransmitted
+import org.elasticsearch.action.ActionListener
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.unit.TimeValue
+import scala.util.{Failure, Success}
 import scala.io.Source
-import scala.concurrent.{ExecutionContext, Future, Promise}
-
-import scala.concurrent.blocking
+import scala.concurrent.duration._
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Hoolix 2017
@@ -31,42 +35,105 @@ object CreateIndexFlow {
       println("=============================================================")
       println(event)
       println(event.indexType)
-      val mapping = Source.fromFile("conf/mapping/" + event.indexType + ".mapping.json").mkString
-      println("read mapping")
+
       val p = Promise[KafkaTransmitted]()
 
-      Future {
-//        blocking {
-          val existsResponse = esClient.admin().indices()
-            .prepareExists(event.indexName).get(defaultTimeout)
-          println("check exist is " + existsResponse.isExists)
-          if (!existsResponse.isExists) {
-            println(event.indexName)
-            println(event.indexType)
-            val createResponse = esClient.admin().indices()
-              .prepareCreate(event.indexName)
-              .setSettings(
-                Settings.builder()
-                  .put("index.refresh_interval", "5s")
-                  .put("index.number_of_shards", "3")
-                  .put("index.number_of_replicas", "1")
-                  //                .put("index.routing.allocation.require.box_type", "hot")
-//                  .put("index.requests.cache.enable", "true")
-              )
-                          .addMapping("_default_", defaultMapping)
-//              .addMapping(event.indexType, mapping)
-              .get(defaultTimeout)
-            println(">>>>>>")
-            println(createResponse)
-            p.success(event)
-          } else {
-            p.success(event)
-          }
-//        }
+
+      def exist(): Future[IndicesExistsResponse] = {
+        val promise = Promise[IndicesExistsResponse]()
+        val listener = new ActionListener[IndicesExistsResponse] {
+          override def onResponse(response: IndicesExistsResponse) = promise.success(response)
+
+          override def onFailure(e: Exception) = promise.failure(e)
+        }
+        esClient.admin().indices().prepareExists(event.indexName).execute(listener)
+        promise.future
       }
-      println("finish create?")
+
+      def create(): Future[CreateIndexResponse] = {
+        val settings: Settings.Builder = Settings.builder()
+          .put("index.refresh_interval", "5s")
+          .put("index.number_of_shards", "3")
+          .put("index.number_of_replicas", "1")
+          .put("index.requests.cache.enable", "true")
+        val mapping = Source.fromFile("conf/mapping/" + event.indexType + ".mapping.json").mkString
+        val promise = Promise[CreateIndexResponse]()
+        val listener = new ActionListener[CreateIndexResponse] {
+          override def onResponse(response: CreateIndexResponse) = promise.success(response)
+          override def onFailure(e: Exception) = promise.failure(e)
+        }
+        esClient.admin().indices().prepareCreate(event.indexName)
+          .setSettings(settings)
+          .addMapping(event.indexType, mapping)
+          .execute(listener)
+        promise.future
+      }
+
+
+      Future {
+        val futureExist = exist()
+        futureExist.onComplete({
+          case Success(valueExist) => {
+            if (valueExist.isExists) {
+              p.success(event)
+            } else {
+              val futureCreate = create()
+              futureCreate.onComplete({
+                case Success(valueCreate) => {
+                  p.success(event)
+                }
+                case Failure(e) => e.printStackTrace()
+              })
+            }
+          }
+          case Failure(e) => e.printStackTrace()
+        })
+      }
 
       p.future
+
+
+
+
+
+
+
+//      val p = Promise[KafkaTransmitted]()
+//      Future {
+//        val existsResponse = esClient.admin().indices()
+//          .prepareExists(event.indexName).get(defaultTimeout)
+//        if (!existsResponse.isExists) {
+//          p.success(event)
+//        } else {
+//          p.success(event)
+//        }
+//      }
+
+//      Future {
+////        blocking {
+//          val existsResponse = esClient.admin().indices()
+//            .prepareExists(event.indexName).get(defaultTimeout)
+//          println("check exist is " + existsResponse.isExists)
+//          if (!existsResponse.isExists) {
+//            println(event.indexName)
+//            println(event.indexType)
+//            val createResponse = esClient.admin().indices()
+//              .prepareCreate(event.indexName)
+//              .setSettings(settings)
+////                          .addMapping("_default_", defaultMapping)
+//              .addMapping(event.indexType, mapping)
+//              .get(defaultTimeout)
+//            println(">>>>>>")
+//            println(createResponse)
+//            p.success(event)
+//          } else {
+//            p.success(event)
+//          }
+////        }
+//      }
+//      println("finish create?")
+//
+//      p.future
 
     }
   }
